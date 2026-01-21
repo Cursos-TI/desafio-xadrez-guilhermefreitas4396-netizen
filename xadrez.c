3285a37d417e9f54a3d05ce6f23349f36cfc71a2
guilhermefreitas4396-netizen <guilhermefreitas4396@gmail.com>
2026-01-21 23:37:49 +0000

    loop aninhado

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,7 @@ int main(){
 int torre;
 int bispo = 1;
 int rainha = 1;
+int cavalo;
 
 //SIMULAÇÃO MOVIMENTO TORRE 
 //FUNÇÃO FOR
@@ -34,6 +35,20 @@ do
     printf("esquerda \n");
     rainha++;
 } while (rainha <= 8);
+
+
+ // Movimento do Cavalo (loops aninhados)
+    printf("\nMovimento do Cavalo:\n");
+
+    for (cavalo = 0; cavalo < 2; cavalo++) {
+        printf("Baixo\n");
+    }
+
+    int lado = 0;
+    while (lado < 1) {
+        printf("Esquerda\n");
+        lado++;
+    }
 
 
 
